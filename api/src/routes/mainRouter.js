7ab0cf4c293232048a8d8a2ba373ad51b4c1f447
Nayrb29 <bryan201429@gmail.com>
2023-07-07 09:56:11 -0500

    Back

--- api/src/routes/mainRouter.js
+++ api/src/routes/mainRouter.js
@@ -1,7 +1,6 @@
 const { Router } = require('express');
 // Importar todos los routers MODULARIZADOS;
 // Ejemplo: const authRouter = require('./auth.js');
-const authRouter = require('./auth');
 const videogamesRouter=require('./videogamesRoute');
 const genresRouter=require('./genresRoute');
 
@@ -10,7 +9,6 @@ const mainRouter = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-mainRouter.use('/auth', authRouter);
 mainRouter.use('/videogames',videogamesRouter);
 mainRouter.use('/genres',genresRouter);
 
